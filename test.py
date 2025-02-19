8b104aa07dc25b012938c2680465176ab9cbfdf3
ente24g <alisijaanton@gmail.com>
2025-02-19 17:13:57 +0100

    jenjing

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print()
+print("вхіт на сторінку топ хек")
+registration = neme_of_user = password_of_user = input("вкажіть імя та пароль щоб ввійти в аккунт")
+Pass_verification = input("найдіть 3 велосипеда на картинці")
+print(f"Ви успішно увішли в аккаунт доброго дня Антон,{neme_of_user = password_of_user})
